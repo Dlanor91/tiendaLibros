761c68aa9e092dd989e9e28a394670247f07619c
Ronald Lima <rl8506@gmail.com>
2025-10-08 23:20:10 -0300

    creando el proyecto

---
+++ APIs/src/main/java/gm/tienda_libros/ServletInitializer.java
@@ -0,0 +1,13 @@
+package gm.tienda_libros;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TiendaLibrosApplication.class);
+	}
+
+}
